701c146145b599722c7a5a5ecb6670a871a8c9ed
abanoub899 <abanobgayed@outlook.com>
2021-05-27 19:28:14 +0200

    aba

--- resources/views/front/product.blade.php
+++ resources/views/front/product.blade.php
@@ -1,22 +1,40 @@
-{{-- @extends('front.nav') --}}
-<div id="demo" class="carousel slide" data-ride="carousel">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Product</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="{{ URL::asset('/css/login.css') }}" />
+</head>
+<body>
+  <div id="demo" class="carousel slide" data-ride="carousel">
 
     <!-- Indicators -->
     <ul class="carousel-indicators">
-      <li data-target="#demo" data-slide-to="0" class="active">Page 1</li>
-      <li data-target="#demo" data-slide-to="1">Page2</li>
-      <li data-target="#demo" data-slide-to="2">Page3</li>
+      <li data-target="#demo" data-slide-to="0"></li>
     </ul>
   
     <!-- The slideshow -->
     <div class="carousel-inner">
       @foreach ($products as $item)
-      <div class="carousel-item active"{{$item['id'==1?'active':'']}}>
+      <div class="carousel-item active"{{$item['id']}}>
         <img src="{{ asset('images/1.jpg') }}" alt="Los Angeles">
-        <h3>{{$item['name']}}</h3>
+        {{-- <h5>{{$item['description']}}</h5> --}}
       </div>  
       @endforeach
-      
       {{-- <div class="carousel-item">
         <img src="chicago.jpg" alt="Chicago">
       </div>
@@ -34,3 +52,5 @@
     </a>
   
   </div>
+</body>
+</html>
