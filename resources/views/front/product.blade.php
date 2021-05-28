68bbe2cf5d0f2887ac40771b51ad1d75fe75ee45
abanoub899 <abanobgayed@outlook.com>
2021-05-28 16:51:53 +0200

    aba

--- resources/views/front/product.blade.php
+++ resources/views/front/product.blade.php
@@ -31,16 +31,16 @@
     <div class="carousel-inner">
       @foreach ($products as $item)
       <div class="carousel-item active"{{$item['id']}}>
-        <img src="{{ asset('images/1.jpg') }}" alt="Los Angeles">
-        {{-- <h5>{{$item['description']}}</h5> --}}
+        <img src="{{ asset('images')}}/{{ $item->gallery}}" alt="Los Angeles">
+        {{-- <h5 class="text-primary">{{$item['description']}}</h5> --}}
       </div>  
       @endforeach
-      {{-- <div class="carousel-item">
-        <img src="chicago.jpg" alt="Chicago">
+      <div class="carousel-item">
+        <img src="{{ asset('images')}}/{{ $item->gallery}}" alt="Chicago">
       </div>
       <div class="carousel-item">
-        <img src="ny.jpg" alt="New York">
-      </div> --}}
+        <img src="{{ asset('images')}}/{{ $item->gallery}}" alt="New York">
+      </div>
     </div>
   
     <!-- Left and right controls -->
@@ -50,7 +50,24 @@
     <a class="carousel-control-next" href="#demo" data-slide="next">
       <span class="carousel-control-next-icon"></span>
     </a>
-  
   </div>
+  <div class="trending-wrapper">
+    <h3>trending products</h3>
+    @foreach ($products as $item)
+    <div class="carousel-item active"{{$item['id']}}>
+      <img src="{{ asset('images')}}/{{ $item->gallery}}" class="img1" alt="Los Angeles">
+      {{-- <h5 class="text-primary">{{$item['name']}}</h5> --}}
+    </div> 
+    @endforeach
+  </div>
+  <style>
+    .img1{
+      height: 100px;
+    }
+    .trending-wrapper{
+      float: left;
+    }
+
+  </style>
 </body>
 </html>
